eeefc0ae9413bb4346766652abc7cdd3da8e0aa2
porridgechou <mrzfengying@163.com>
2023-01-27 21:51:21 +0800

    0127

--- BinarySearch/searchRange.cpp
+++ BinarySearch/searchRange.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 class Solution {
 public:
-    int left_bound(vector<int>& nums, int target) {
+    int left_bound (vector<int>& nums, int target) {
         int left = 0;
         int right = nums.size() - 1;
         while (left <= right)
@@ -13,19 +13,18 @@ public:
             if (nums[mid] < target) {
                 left = mid + 1;
             } else if (nums[mid] > target) {
-                right = mid - 1;
-            } else if (nums[mid] == target) {
-                right = mid - 1;
+                right = mid -1;
+            } else {
+                right = mid -1;
             }
         }
-        if (left == nums.size()) {
+        if (left == nums.size()){
             return -1;
         }
-
         return nums[left] == target ? left : -1;
     }
 
-    int right_bound(vector<int>& nums, int target) {
+    int right_bound (vector<int>& nums, int target) {
         int left = 0;
         int right = nums.size() - 1;
         while (left <= right)
@@ -35,39 +34,17 @@ public:
                 left = mid + 1;
             } else if (nums[mid] > target) {
                 right = mid - 1;
-            } else if (nums[mid] == target) {
+            } else {
                 left = mid + 1;
             }
         }
-        if (left - 1 < 0) {
+        if (right < 0){
             return -1;
         }
-
-        return nums[left - 1] == target ? (left - 1) : -1;
+        return nums[right] == target ? right : -1;
     }
-
+    
     vector<int> searchRange(vector<int>& nums, int target) {
         return vector<int> {left_bound(nums, target), right_bound(nums, target)};
     }
 };
-
-
-
-    int left_bound(vector<int>& nums, int target) {
-        int left = 0;
-        int right = nums.size();
-        while (left < right)
-        {
-            int mid = left + (right - left) / 2;
-            if (nums[mid] == target) {
-                right = mid;
-            } else if (nums[mid] < target) {
-                left = mid + 1;
-            } else if (nums[mid] > target)
-            {
-                right = mid;
-            }
-            return left;
-        }
-        
-    }
